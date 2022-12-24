a39b12e148f8c4c05ebb32c5ada37f4466b5a3ff
YuKung <2368384805@qq.com>
2022-12-24 11:00:26 +0800

    添加了people基类

---
+++ people.h
@@ -0,0 +1,22 @@
+//
+// Created by YuKung on 2022/12/24.
+//
+
+#ifndef FINALEXAM_PEOPLE_H
+#define FINALEXAM_PEOPLE_H
+
+class Cpeople {
+
+private:
+    int IDNumber;
+    bool gender;                    // 0代表男，1代表女
+    int age;
+
+public:
+    Cpeople();
+    ~Cpeople();
+    
+};
+
+
+#endif //FINALEXAM_PEOPLE_H
